f820fa83078ce470ced352b02d89cb03db569bd1
bigwiggerz <bigwiggeroffical@gmail.com>
2023-05-27 22:33:39 +0300

    edit some files

--- app/src/main/java/com/topic3/android/reddit/screens/ChatActivity.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/ChatActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import java.util.Locale
 import com.topic3.android.reddit.databinding.ActivityChatBinding
 
@@ -34,40 +35,42 @@ class ChatActivity : AppCompatActivity() {
             finish()
         }
 
-        binding.button.setOnClickListener {
+        binding.composeButton.setOnClickListener {
             showToast()
         }
     }
 
-    private fun showToast() {
-        Toast.makeText(this, "Imaginary chat started!", Toast.LENGTH_SHORT).show()
-    }
-}
-@Composable
-private fun ComposableButton(onButtonClick: () -> Unit){
-    val buttonColors = buttonColors(
-        backgroundColor = Color(0xFF006837),
-        contentColor = Color.White
-    )
 
-    Button(
-        onClick = onButtonClick, elevation = null,
-        shape = RoundedCornerShape(corner = CornerSize(24.dp)),
-        contentPadding = PaddingValues(start = 32.dp, end = 32.dp),
-        colors = buttonColors,
-        modifier = Modifier.height(48.dp)
-    ) {
-        Text(
-            text = "Start chatting".toUpperCase(Locale.US),
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Medium
+        private fun showToast() {
+            Toast.makeText(this, "Imaginary chat started!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @Composable
+    private fun ComposableButton(onButtonClick: () -> Unit) {
+        val buttonColors = buttonColors(
+            backgroundColor = Color(0xFF006837),
+            contentColor = Color.White
         )
-    }
-}
 
-@Preview
-@Composable
-private fun ComposeButtonPreview() {
-    ComposableButton {
+        Button(
+            onClick = onButtonClick, elevation = null,
+            shape = RoundedCornerShape(corner = CornerSize(24.dp)),
+            contentPadding = PaddingValues(start = 32.dp, end = 32.dp),
+            colors = buttonColors,
+            modifier = Modifier.height(48.dp)
+        ) {
+            Text(
+                text = "Start chatting".toUpperCase(Locale.US),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium
+            )
+        }
     }
-}
+
+    @Preview
+    @Composable
+    private fun ComposeButtonPreview() {
+        ComposableButton {
+        }
+    }
